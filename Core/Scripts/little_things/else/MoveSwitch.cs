28d94df06b144a9df0e03c3f68bf4d049cef3ae1
Fungus-Light <542461670@qq.com>
2020-04-15 12:35:15 +0800

    角色移动关闭

---
+++ Core/Scripts/little_things/else/MoveSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Black_Rabbit;
+
+//一个简单的切换移动的小补丁，不建议使用
+public class MoveSwitch : MonoBehaviour
+{
+    public CharacterMovement movement;
+    
+    void Start()
+    {
+        movement = GetComponent<CharacterMovement>();
+    }
+
+    public void StopMove()
+    {
+        movement.enabled = false;
+    }
+
+    public void ActiveMove()
+    {
+        movement.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
