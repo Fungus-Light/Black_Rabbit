3c56f5950a48b04788aca6cb169008d3e5e34821
Fungus-Light <542461670@qq.com>
2020-04-18 22:07:50 +0800

    almost finish 0.0.3

---
+++ Core/Scripts/FPS/FlashLightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightManager : MonoBehaviour
+{
+    public FlashLight flashlight;
+    public KeyCode key=KeyCode.F;
+
+    void Start()
+    {
+        flashlight = FindObjectOfType<FlashLight>();
+        flashlight.gameObject.SetActive(isOn);
+    }
+
+    bool isOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(key))
+        {
+            isOn = !isOn;
+            flashlight.gameObject.SetActive(isOn);
+
+        }
+    }
+
+    public void FlashOn()
+    {
+        isOn = true;
+        flashlight.gameObject.SetActive(isOn);
+    }
+
+    public void FlashOff()
+    {
+        isOn = false;
+        flashlight.gameObject.SetActive(isOn);
+    }
+
+}
